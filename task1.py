c4ad28274e04cf0a765c81fbcddc463cd971aa56
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-02-02 15:21:44 -0800

    dshsdy

--- task1.py
+++ task1.py
@@ -1,19 +1,25 @@
 #!python3
 
+from tkinter import Text, Tk
 import tkinter as tk 
 from tkinter import *
 
 window = tk.Tk()
 window.title("tk")
 window.geometry("400x40")
-
 label = tk.Label(window, text="x")
 label.pack()
-
 label2 = tk.Label(window, text="=")
 label2.pack()
-
-label2.place(x=21,y=45)
-label.place(x=23,y=50)
-
+label2.place(x=165,y=6)
+label.place(x=70,y=6)
+t = Text(window, height=1, width=5)
+t.place(x=10,y=6)
+t = Text(window, height=1, width=5)
+t.place(x=100,y=6)
+t = Text(window, height=1, width=20)
+t.place(x=200,y=6)
 window.mainloop()
+
+
+
