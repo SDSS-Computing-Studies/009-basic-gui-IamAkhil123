67ccc54338e8e19ebe47fb18c8b31ae263f6e386
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-02-01 16:55:52 -0800

    asdf

--- task1.py
+++ task1.py
@@ -1,32 +1,49 @@
 #!python3
 import tkinter as tk 
 from tkinter import *
-import tkinter as ttk
+from tkinter import ttk
 
 
+"""
 window = tk.Tk()
 window.title("Hi!")
 window.geometry("300x50")
 L1 = Label(window, text="=")
-L1.pack( side = LEFT)
+L1.pack( center(side))
 E1 = Entry(window, bd =1)
 E1.pack(side = RIGHT)
 E1 = Entry(window, bd =1)
 E1.pack(side = LEFT)
-E1 = Entry(window, bd =1)
-E1.pack(side = bottom)
+
 
 
 window.mainloop()
+"""
 
-label = ttk.Label(window, text = "x")
-label.grid(column = 0, row = 0)
-name = tk.StringVar()
-nameEntered = ttk.Entry(window, width = 15, textvariable = name)
-nameEntered.grid(column = 0, row = 1)
 
-text_box = Text(
-    height=12,
-    width=40
-)
-text_box.pack()
+
+window = tk.Tk()
+window.title("Packing Widgets example")
+
+window.resizable(False,False)
+
+label1 = tk.Label(window,text="x")
+
+dogphoto = PhotoImage(file="dog.png")
+label2 = tk.Label(window, image=dogphoto, borderwidth=3, relief=SUNKEN)
+
+lable3 = tk.Label(window, text="Note that an image can be in a label or a button!", borderwidth=4, relief=SUNKEN)
+
+button1 = tk.Button(window,text="A button\nis clickable")
+
+entry1 = tk.Entry(window,text="Entry widgets can be typed in", borderwidth=3, relief=SUNKEN)
+
+combo = ttk.Combobox(window,values=["1","2","3"])
+
+label1.grid(row = 1, column = 1)
+label2.grid(row = 1, column = 2, rowspan = 2)
+lable3.grid(row = 2, column = 1)
+
+entry1.grid(row=3, column = 1, columnspan=2)
+
+window.mainloop()
