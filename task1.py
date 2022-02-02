ab489cabcfdeeb9b5aadb5ba8937c55cc1e3a22e
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-02-01 16:48:00 -0800

    rftyhd

--- task1.py
+++ task1.py
@@ -1,27 +1,32 @@
 #!python3
-
+import tkinter as tk 
 from tkinter import *
- 
-def addNumbers():
-    res=int(e1.get())+int(e2.get())
-    myText.set(res)
- 
-master = Tk()
-myText=StringVar()
-Label(master, text="First").grid(row=0, sticky=W)
-Label(master, text="Second").grid(row=1, sticky=W)
-Label(master, text="Result:").grid(row=3, sticky=W)
-result=Label(master, text="", textvariable=myText).grid(row=3,column=1, sticky=W)
- 
-e1 = Entry(master)
-e2 = Entry(master)
- 
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
- 
-b = Button(master, text="Calculate", command=addNumbers)
-b.grid(row=0, column=2,columnspan=2, rowspan=2,sticky=W+E+N+S, padx=5, pady=5)
- 
- 
-mainloop()
- 
+import tkinter as ttk
+
+
+window = tk.Tk()
+window.title("Hi!")
+window.geometry("300x50")
+L1 = Label(window, text="=")
+L1.pack( side = LEFT)
+E1 = Entry(window, bd =1)
+E1.pack(side = RIGHT)
+E1 = Entry(window, bd =1)
+E1.pack(side = LEFT)
+E1 = Entry(window, bd =1)
+E1.pack(side = bottom)
+
+
+window.mainloop()
+
+label = ttk.Label(window, text = "x")
+label.grid(column = 0, row = 0)
+name = tk.StringVar()
+nameEntered = ttk.Entry(window, width = 15, textvariable = name)
+nameEntered.grid(column = 0, row = 1)
+
+text_box = Text(
+    height=12,
+    width=40
+)
+text_box.pack()
