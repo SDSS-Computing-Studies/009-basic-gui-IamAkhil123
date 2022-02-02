a10357fa379064b18db14f4247926578d758dd80
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-02-01 18:39:30 -0800

    update

--- task1.py
+++ task1.py
@@ -1,49 +1,19 @@
 #!python3
+
 import tkinter as tk 
 from tkinter import *
-from tkinter import ttk
-
-
-"""
-window = tk.Tk()
-window.title("Hi!")
-window.geometry("300x50")
-L1 = Label(window, text="=")
-L1.pack( center(side))
-E1 = Entry(window, bd =1)
-E1.pack(side = RIGHT)
-E1 = Entry(window, bd =1)
-E1.pack(side = LEFT)
-
-
-
-window.mainloop()
-"""
-
-
 
 window = tk.Tk()
-window.title("Packing Widgets example")
+window.title("tk")
+window.geometry("400x40")
 
-window.resizable(False,False)
+label = tk.Label(window, text="x")
+label.pack()
 
-label1 = tk.Label(window,text="x")
+label2 = tk.Label(window, text="=")
+label2.pack()
 
-dogphoto = PhotoImage(file="dog.png")
-label2 = tk.Label(window, image=dogphoto, borderwidth=3, relief=SUNKEN)
-
-lable3 = tk.Label(window, text="Note that an image can be in a label or a button!", borderwidth=4, relief=SUNKEN)
-
-button1 = tk.Button(window,text="A button\nis clickable")
-
-entry1 = tk.Entry(window,text="Entry widgets can be typed in", borderwidth=3, relief=SUNKEN)
-
-combo = ttk.Combobox(window,values=["1","2","3"])
-
-label1.grid(row = 1, column = 1)
-label2.grid(row = 1, column = 2, rowspan = 2)
-lable3.grid(row = 2, column = 1)
-
-entry1.grid(row=3, column = 1, columnspan=2)
+label2.place(x=21,y=45)
+label.place(x=23,y=50)
 
 window.mainloop()
